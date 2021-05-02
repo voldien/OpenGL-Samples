0c12dec195375040bd3c625d16c85acb86c2bc1d
Valdemar Lindberg <voldiekami@gmail.com>
2021-05-02 20:39:43 +0200

    Updated common

---
+++ common/GLWindow.cpp
@@ -0,0 +1,186 @@
+#include "GLWindow.h"
+#include "VKHelper.h"
+#include "common.hpp"
+#include <SDL2/SDL_vulkan.h>
+#include <cassert>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
+
+GLWindow::~GLWindow(void) { this->close(); }
+
+GLWindow::GLWindow(const VulkanCore &core, int x, int y, int width, int height) {
+
+	if (!SDL_InitSubSystem(SDL_INIT_EVENTS | SDL_INIT_VIDEO)) {
+		throw std::runtime_error(fvformatf("failed create window - %s", SDL_GetError()));
+	}
+
+	SDL_DisplayMode displaymode;
+	SDL_GetCurrentDisplayMode(0, &displaymode);
+	if (x == -1 && y == -1) {
+		x = displaymode.w / 4;
+		y = displaymode.h / 4;
+	}
+
+	if (width == -1 && height == -1) {
+		width = displaymode.w / 2;
+		height = displaymode.h / 2;
+	}
+
+	/*  Create Vulkan window.   */
+	this->window = SDL_CreateWindow("Vulkan Sample", x, y, width, height, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+	if (window == NULL) {
+		throw std::runtime_error(fvformatf("failed create window - %s", SDL_GetError()));
+	}
+
+	createSwapChain();
+	/*	*/
+	this->Initialize();
+}
+
+void GLWindow::swapBuffer(void) { SDL_GL_SwapWindow(this->window) }
+
+void GLWindow::createSwapChain(void) { glcontext = SDL_GL_CreateContext(this->window); }
+
+void GLWindow::recreateSwapChain(void) {
+	vkDeviceWaitIdle(getDevice());
+
+	cleanSwapChain();
+
+	createSwapChain();
+}
+
+void GLWindow::cleanSwapChain(void) {
+	for (auto framebuffer : swapChain->swapChainFramebuffers) {
+		vkDestroyFramebuffer(core->device, framebuffer, nullptr);
+	}
+
+	vkFreeCommandBuffers(core->device, this->cmd_pool, static_cast<uint32_t>(swapChain->commandBuffers.size()),
+						 swapChain->commandBuffers.data());
+
+	vkDestroyRenderPass(getDevice(), swapChain->renderPass, nullptr);
+
+	for (auto imageView : swapChain->swapChainImageViews) {
+		vkDestroyImageView(core->device, imageView, nullptr);
+	}
+
+	vkDestroySwapchainKHR(core->device, this->swapChain->swapchain, nullptr);
+}
+
+void GLWindow::show() { SDL_ShowWindow(this->window); }
+
+void GLWindow::hide() { SDL_HideWindow(this->window); }
+
+void GLWindow::close(void) {
+	this->hide();
+	SDL_DestroyWindow(this->window);
+}
+
+void GLWindow::setPosition(int x, int y) { SDL_SetWindowPosition(this->window, x, y); }
+
+void GLWindow::setSize(int width, int height) {
+	/*	TODO determine if it shall update framebuffera as well.	*/
+	SDL_SetWindowSize(this->window, width, height);
+}
+
+void GLWindow::vsync(bool state) {}
+
+// GLWindow::~RendererWindow(void) {}
+
+void GLWindow::getPosition(int *x, int *y) const { SDL_GetWindowPosition(this->window, x, y); }
+
+void GLWindow::getSize(int *width, int *height) const { SDL_GetWindowSize(this->window, width, height); }
+
+void GLWindow::setTitle(const char *title) { SDL_SetWindowTitle(this->window, title); }
+
+std::string &GLWindow::getTitle(void) const{
+
+}
+std::string GLWindow::getTitle(void){
+
+}
+
+void GLWindow::resizable(bool resizable) { SDL_SetWindowResizable(this->window, (SDL_bool)resizable); }
+
+void GLWindow::setFullScreen(bool fullscreen) {
+	// TODO add option for using either of the modes.
+	if (fullscreen)
+		SDL_SetWindowFullscreen(this->window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	else
+		SDL_SetWindowFullscreen(this->window, 0);
+}
+
+bool GLWindow::isFullScreen(void) const {}
+
+void GLWindow::setBordered(bool bordered) { SDL_SetWindowBordered(this->window, (SDL_bool)bordered); }
+
+int GLWindow::width(void) const {
+	int w, h;
+	getSize(&w, &h);
+	return w;
+}
+int GLWindow::height(void) const {
+	int w, h;
+	getSize(&w, &h);
+	return h;
+}
+
+void GLWindow::setMinimumSize(int width, int height) { SDL_SetWindowMinimumSize(this->window, width, height); }
+void GLWindow::getMinimumSize(int *width, int *height) {}
+
+void GLWindow::setMaximumSize(int width, int height) { SDL_SetWindowMaximumSize(this->window, width, height); }
+void GLWindow::getMaximumSize(int *width, int *height) {}
+
+void GLWindow::focus(void) { SDL_SetWindowInputFocus(this->window); }
+
+void GLWindow::restore(void) { SDL_RestoreWindow(this->window); }
+
+void GLWindow::maximize(void) { SDL_MaximizeWindow(this->window); }
+
+void GLWindow::minimize(void) { SDL_MinimizeWindow(this->window); }
+
+intptr_t GLWindow::getNativePtr(void) const {}
+
+void GLWindow::Initialize(void) {}
+
+void GLWindow::draw(void) {}
+
+void GLWindow::onResize(int width, int height) { recreateSwapChain(); }
+
+void GLWindow::run(void) {
+	SDL_Event event = {};
+	bool isAlive = true;
+	bool visible;
+
+	while (isAlive) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				return; /*  Exit.  */
+			case SDL_KEYDOWN:
+				break;
+			case SDL_WINDOWEVENT:
+				switch (event.window.event) {
+				case SDL_WINDOWEVENT_CLOSE:
+					return;
+				case SDL_WINDOWEVENT_SIZE_CHANGED:
+				case SDL_WINDOWEVENT_RESIZED:
+					onResize(event.window.data1, event.window.data2);
+				case SDL_WINDOWEVENT_HIDDEN:
+				case SDL_WINDOWEVENT_MINIMIZED:
+					visible = 0;
+					break;
+				case SDL_WINDOWEVENT_EXPOSED:
+				case SDL_WINDOWEVENT_SHOWN:
+					visible = 1;
+					break;
+				}
+				break;
+
+			default:
+				break;
+			}
+			this->draw();
+			this->swapBuffer();
+		}
+	}
+}
---
+++ common/GLWindow.h
@@ -0,0 +1,102 @@
+#ifndef _STARTUP_WINDOW_SAMPLE_H_
+#define _STARTUP_WINDOW_SAMPLE_H_ 1
+#include "SDLWindow.h"
+#include <GL/glew.h>
+#include <SDL2/SDL_video.h>
+#include <memory>
+#include <vector>
+
+class GLWindow : public SDLWindow {
+  public:
+	GLWindow(int x, int y, int width, int height);
+	GLWindow(const GLWindow &other) = delete;
+	~GLWindow(void);
+
+  public:
+	/**
+	 * @brief
+	 *
+	 */
+	virtual void Initialize(void);
+	/**
+	 * @brief
+	 *
+	 */
+	virtual void Release(void);
+	/**
+	 * @brief
+	 *
+	 */
+	virtual void draw(void);
+	/**
+	 * @brief
+	 *
+	 */
+	virtual void run(void);
+
+	/**
+	 * @brief
+	 *
+	 */
+	virtual void onResize(int width, int height);
+
+  public:
+	virtual void swapBuffer(void);
+
+	virtual void close(void);
+
+	virtual void show(void);
+
+	virtual void hide(void);
+
+	virtual void focus(void);
+
+	virtual void restore(void);
+
+	virtual void maximize(void);
+
+	virtual void minimize(void);
+
+	virtual void setPosition(int x, int y);
+
+	virtual void setSize(int width, int height);
+
+	virtual void vsync(bool state);
+
+	virtual void getPosition(int *x, int *y) const;
+
+	virtual void getSize(int *width, int *height) const;
+
+	virtual void resizable(bool resizable);
+
+	virtual void setFullScreen(bool fullscreen);
+
+	virtual bool isFullScreen(void) const;
+
+	virtual void setBordered(bool bordered);
+
+	virtual int width(void) const;
+	virtual int height(void) const;
+
+	virtual void setMinimumSize(int width, int height);
+	virtual void getMinimumSize(int *width, int *height);
+	virtual void setMaximumSize(int width, int height);
+	virtual void getMaximumSize(int *width, int *height);
+
+	virtual void setTitle(const char *title);
+	virtual std::string &getTitle(void) const;
+	virtual std::string getTitle(void);
+
+	virtual intptr_t getNativePtr(void) const;
+
+  protected:
+	virtual void createSwapChain(void);
+	virtual void recreateSwapChain(void);
+	virtual void cleanSwapChain(void);
+
+  private:
+	SDL_Window *window;
+	SDL_GLContext glcontext;
+};
+
+#endif
