e57a62c6bd37ed26fd782b73d87c4557e5518bf1
Valdemar Lindberg <voldiekami@gmail.com>
2021-05-02 20:39:56 +0200

    Removed

--- common/SampleCommon.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_ 1
-#include<SDL2/SDL.h>
-
-extern void initWindowManager();
-extern SDL_Window *createWindow(const char *ctitle);
-extern void deleteWindow(SDL_Window* window);
-
-#endif
