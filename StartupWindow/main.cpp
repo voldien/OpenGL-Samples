614da35a559b1d3a1254df42e4fa923798953a16
Valdemar Lindberg <voldiekami@gmail.com>
2021-05-02 20:40:21 +0200

    Updated

--- StartupWindow/main.cpp
+++ StartupWindow/main.cpp
@@ -1,63 +1,36 @@
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_video.h>
-#include<GL/glew.h>
-#include"SampleCommon.h"
+#include "GLWindow.h"
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <iostream>
 
-int main(int argc, const char** argv) {
+class StartUpWindow : public GLWindow {
+  public:
+	StartUpWindow() : GLWindow(-1, -1, -1, -1) {}
 
-	int isAlive = 1;
-	int visible = 0;
-	SDL_Event event = {};
+	virtual void Initialize(void) { glClearColor(0.2f, 0.2f, 0.2f, 1.0f); }
 
+	virtual void draw(void) {
 
-	initWindowManager();
-
-	/*	*/
-	SDL_Window* window = createWindow("OpenGL Window");
-	SDL_ShowWindow(window);
-
-	glClearColor(0.5f,0.5f,0.5f,1.0f);
-
-
-	while (isAlive) {
-		while (SDL_PollEvent(&event))
-		{
-			switch (event.type)
-			{
-			case SDL_APP_TERMINATING:
-			case SDL_QUIT:
-				isAlive = 0;
-				break;
-			case SDL_WINDOWEVENT:
-				switch (event.window.type) {
-				case SDL_WINDOWEVENT_CLOSE:
-					isAlive = SDL_FALSE;
-					goto finish;
-				case SDL_WINDOWEVENT_SIZE_CHANGED:
-				case SDL_WINDOWEVENT_RESIZED:
-					glViewport(0, 0, event.window.data1, event.window.data2);
-					printf("viewport: %dx%d\n", event.window.data1, event.window.data2);
-					break;
-				case SDL_WINDOWEVENT_HIDDEN:
-					visible = 0;
-					break;
-				case SDL_WINDOWEVENT_EXPOSED:
-				case SDL_WINDOWEVENT_SHOWN:
-					visible = 1;
-					break;
-				}
-				/* code */
-				break;
-			default:
-				break;
-			}
-		}
+		int w, h;
+		getSize(&w, &h);
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(window);
 	}
 
-finish:
-	deleteWindow(window);
-	SDL_Quit();
+	virtual void update(void) {}
+};
+
+int main(int argc, const char **argv) {
+	try {
+
+		//	OpenGLCore core(argc, argv);
+		StartUpWindow w();
+
+		w.run();
+	} catch (std::exception &ex) {
+		std::cerr << ex.what();
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
